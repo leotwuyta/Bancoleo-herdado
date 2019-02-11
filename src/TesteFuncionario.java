c03592bd23eb1e9c101ffa095b5476c67822a194
Leonardo Reis <leonardoreissousa95@gmail.com>
2019-02-11 10:59:12 -0200

    Subindo projeto para o github

---
+++ src/TesteFuncionario.java
@@ -0,0 +1,15 @@
+
+public class TesteFuncionario {
+
+	public static void main(String[] args) {
+		
+		FuncionarioTeste f1 = new FuncionarioTeste();
+		f1.setSalario(3000.0);
+		System.out.println(f1.getTipo());
+		System.out.println(f1.getBonificacao());
+		
+		
+
+	}
+
+}
